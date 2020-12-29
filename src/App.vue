d677abfe135692b5e4c4aeed6134e962a1a9f1f2
Silyow <515185930@qq.com>
2020-12-29 16:04:42 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,30 @@
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+/* 解决ios用户设置字体大小 */
+body {
+  -webkit-text-size-adjust: 100% !important;
+}
 </style>
+<script>
+//解决安卓用户设置字体大小
+/* eslint-disable */
+(function() {
+  if (
+    typeof WeixinJSBridge == "object" &&
+    typeof WeixinJSBridge.invoke == "function"
+  ) {
+    handleFontSize();
+  } else {
+    document.addEventListener("WeixinJSBridgeReady", handleFontSize, false);
+  }
+  function handleFontSize() {
+    // 设置网页字体为默认大小
+    WeixinJSBridge.invoke("setFontSizeCallback", { fontSize: 0 });
+    // 重写设置网页字体大小的事件
+    WeixinJSBridge.on("menu:setfont", function() {
+      WeixinJSBridge.invoke("setFontSizeCallback", { fontSize: 0 });
+    });
+  }
+})();
+</script>
